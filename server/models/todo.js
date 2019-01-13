0c451e1f91562da40dabc7c40addb0ccb8abefa8
unknown <kilq2reaper@hotmail.com>
2019-01-13 12:47:03 +0700

    test POST/todos route

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,13 +1,13 @@
 
 let {mongoose} = require('../db/mongoose');
 
-module.exports.Todo = mongoose.model('Todo', {
+let Todo = mongoose.model('Todo', {
     text:{  type: String, required:true, minlength:10, trim:true },
     completed:{ type: Boolean, default:false},
     completedAt:{ type: Number , default:null}
     });
 
-    
+module.exports = {Todo};  
 // let newTodo = new Todo(
     //     {text:'cook dinner'});
     
